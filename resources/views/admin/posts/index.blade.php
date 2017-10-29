432684fe3cd3af41ba16d12239ec093c839da098
Manish <mbasnet911@gmail.com>
2017-10-29 12:10:47 +0545

    ablt to create post with dynamic categories

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -39,7 +39,7 @@
               <td>{{$post->id}}</td>
               <td><img height="50" src="{{$post->photo ? $post->photo->file: 'http://via.placeholder.com/350x150'}}" ></td>
               <td>{{$post->user->name}}</td>
-              <td>{{$post->category_id}}</td>
+              <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
                 <td>{{$post->title}}</td>
                 <td>{{$post->body}}</td>
                 <td>{{$post->created_at->diffForHumans()}}</td>
